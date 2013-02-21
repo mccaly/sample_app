226c2112ca809d28d9657fb36ed563916bd24ea5
Andrew McCalister <andrewmccalister@googlemail.com>
2013-02-20 23:24:57 -0800

    Finish user edit, update, index, and destroy actions

--- spec/requests/authentications_pages_spec.rb
+++ spec/requests/authentications_pages_spec.rb
@@ -52,7 +52,7 @@ end
 
 
 describe "AuthenticationsPages" do
-  describe "GET /authentications_pages" do
+  describe "GET /authentication_pages" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       get authentication_pages_index_path
